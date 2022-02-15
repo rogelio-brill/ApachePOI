8e38b1e8b732a85089fc4d7ba9ffa2fc3dd6f9ca
Rogelio <rogelio.aguilar@brillio.com>
2022-02-14 22:29:08 -0800

    First commit

---
+++ ApachePOI/src/ReadCells.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadCells {
+   static XSSFRow row;
+   
+   public static void main(String[] args) throws Exception {
+      FileInputStream fis = new FileInputStream(new File("C:\\Users\\rogelio.aguilar\\Documents\\Excel\\Writesheet.xlsx"));
+      XSSFWorkbook workbook = new XSSFWorkbook(fis);
+      XSSFSheet spreadsheet = workbook.getSheetAt(0);
+      Iterator < Row >  rowIterator = spreadsheet.iterator();
+      
+      while (rowIterator.hasNext()) {
+         row = (XSSFRow) rowIterator.next();
+         Iterator < Cell >  cellIterator = row.cellIterator();
+         while ( cellIterator.hasNext()) {
+             Cell cell = cellIterator.next();
+             
+             switch (cell.getCellType()) {
+                case NUMERIC:
+                   System.out.print(cell.getNumericCellValue() + " \t\t ");
+                   break;
+                
+                case STRING:
+                   System.out.print(
+                   cell.getStringCellValue() + " \t\t ");
+                   break;
+             }
+          }
+          System.out.println();
+       }
+       fis.close();
+    }
+ }
